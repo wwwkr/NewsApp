9bd7cc196cee0a8187cc17d23e747c22c7e4ca8b
wwwkr <xodn1003@naver.com>
2024-03-28 11:36:37 +0900

    코드정리

--- domain/src/main/java/com/wwwkr/domain/usecase/UpdateMemoUseCase.kt
+++ domain/src/main/java/com/wwwkr/domain/usecase/UpdateMemoUseCase.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.wwwkr.domain.model.ArticleModel
 import com.wwwkr.domain.model.UiState
 import com.wwwkr.domain.repository.NewsRepository
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
@@ -14,6 +15,7 @@ class UpdateMemoUseCase @Inject constructor(private val newsRepository: NewsRepo
 
     private val _getScrapNewsStateFlow = newsRepository.getScrapNewsStateFlow
 
+    @OptIn(FlowPreview::class)
     suspend operator fun invoke(title: String, memo: String) {
         newsRepository.updateNews(title = title, memo = memo)
             .flatMapConcat { newsRepository.selectNews() }
